628944264a83e04c96e8e135ed35ca855ecf9c9b
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-05-20 23:47:15 -0500

    initial mergesort concept
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

---
+++ merge_sort/mergesort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []int{38, 27, 43, 3, 9, 82, 10}
+	final := mergesort(list)
+	fmt.Println(final)
+}
+
+func mergesort(list []int) []int {
+	if len(list) <= 1 {
+		return list
+	}
+
+	split := len(list)/2 + 1
+	left := list[:split]
+	right := list[split:]
+}
+
+func merge(left, right []int) []int {
+
+}
