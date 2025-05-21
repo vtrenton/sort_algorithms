cef24fe148c5c7979f9c391cbc2cba12600f07f1
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-05-21 18:57:25 -0500

    worked on recursive concept of mergesort
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- merge_sort/mergesort.go
+++ merge_sort/mergesort.go
@@ -16,6 +16,11 @@ func mergesort(list []int) []int {
 	split := len(list)/2 + 1
 	left := list[:split]
 	right := list[split:]
+
+	sortedleft := mergesort(left)
+	sortedright := mergesort(right)
+
+	return merge(sortedleft, right)
 }
 
 func merge(left, right []int) []int {
